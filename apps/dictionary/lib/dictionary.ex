d4c0895fd3ac3900408e5c88fda3aff59863e2d5
Bruno Parga <brunoparga@gmail.com>
2022-03-13 23:03:41 +0400

    Turn the Dictionary into an App

--- apps/dictionary/lib/dictionary.ex
+++ apps/dictionary/lib/dictionary.ex
@@ -3,13 +3,6 @@ defmodule Dictionary do
   The dictionary for a Hangman game.
   """
 
-  alias Dictionary.Runtime.Server
-
-  @opaque t :: Server.t()
-
-  @spec start_link() :: {:error, any()} | {:ok, t()}
-  defdelegate start_link, to: Server
-
-  @spec random_word(pid()) :: String.t()
-  defdelegate random_word(pid), to: Server
+  @spec random_word() :: String.t()
+  defdelegate random_word(), to: Dictionary.Runtime.Server
 end
