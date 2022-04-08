fdb15abbff96367a0cc074457e4e30f86e433435
Bruno Parga <brunoparga@gmail.com>
2022-04-08 19:50:51 +0400

    Prevent server timeout

--- hangman-umbrella/apps/hangman/lib/runtime/server.ex
+++ hangman-umbrella/apps/hangman/lib/runtime/server.ex
@@ -1,22 +1,31 @@
 defmodule Hangman.Runtime.Server do
-  alias Hangman.Impl.Game
   use GenServer
+  alias Hangman.{Impl.Game, Runtime.Watchdog}
   @type t :: pid()
+  # 1 hour
+  @idle_timeout 1 * 60 * 60 * 1000
+
+  ### client process ###
 
   def start_link(_args) do
     GenServer.start_link(__MODULE__, nil)
   end
 
+  ### server process ###
+
   def init(_args) do
-    {:ok, Game.new_game()}
+    watcher = Watchdog.start(@idle_timeout)
+    {:ok, {Game.new_game(), watcher}}
   end
 
-  def handle_call({:make_move, guess}, _from, game) do
+  def handle_call({:make_move, guess}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
     {updated_game, tally} = Game.make_move(game, guess)
-    {:reply, tally, updated_game}
+    {:reply, tally, {updated_game, watcher}}
   end
 
-  def handle_call({:tally}, _from, game) do
-    {:reply, Game.tally(game), game}
+  def handle_call({:tally}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
+    {:reply, Game.tally(game), {game, watcher}}
   end
 end
