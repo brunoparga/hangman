48ba5f4fca34bab2525b7b1f8500cda46a201fc3
Bruno Parga <brunoparga@gmail.com>
2022-04-10 04:48:09 +0400

    Remove LiveDashboard

--- browser_2/mix.exs
+++ browser_2/mix.exs
@@ -39,7 +39,6 @@ defmodule Browser2.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
